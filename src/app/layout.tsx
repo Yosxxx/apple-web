9d1e225932f1d49d2fad33d3c3a4f81d56966905
Yos <Yosuamontolaluxd@gmail.com>
2025-06-01 02:46:35 +0700

    fixed the position i guess?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head></head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
